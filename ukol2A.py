ecf7b435e85cb8b3767549edd2460b15a893acca
matyaspetrek <m_petrek@utb.cz>
2026-02-26 12:06:42 +0100

    Okomentování kódu

--- ukol2A.py
+++ ukol2A.py
@@ -1,32 +1,43 @@
 #Magician
 import time
-
+"Nastavení ryhlosti PTP pohybů"
 dType.SetPTPJointParams(api,200,200,200,200,200,200,200,200,0)
 dType.SetPTPCoordinateParams(api,200,200,200,200,0)
 dType.SetPTPJumpParams(api, 10, 200,0)
 dType.SetPTPCommonParams(api, 100, 100,0)
 
+"Nastavení souřadnic skladiště společně i s rotací hlavy robota"
 skladisteX=182
 skladisteY=-45
 skladisteZ=-38
 rHead=-1
 
 
-kostkaVel=25
+kostkaVel=25 ##Velikost kostky
+"Souřadnice věže"
 vezX=218
 vezY=111
 vezZ=-38
-pocet=0
-for i in range(2):
-	for j in range(2):
+
+pocet=0 ##Počet kostek poskládaných do věže
+
+for i in range(2):  ##Smyčka podle počtu kostek ve skladišti v Y souřadnici
+	for j in range(2):  ##Smyčka podle počtu kostek ve skladišti v X souřadnici
+		"Pohyb robota na souřadnici skladiště a zvednutí kostky s přísavkou"
 		dType.SetPTPCmd(api, 2, skladisteX+kostkaVel*j, skladisteY+kostkaVel*i*-1, skladisteZ+30, rHead, 1)
 		dType.SetPTPCmd(api, 2, skladisteX+kostkaVel*j, skladisteY+kostkaVel*i*-1, skladisteZ, rHead, 1)
 		dType.SetEndEffectorSuctionCup(api, 1,  1, 1)
 		dType.SetPTPCmd(api, 2, skladisteX+kostkaVel*j, skladisteY+kostkaVel*i*-1, skladisteZ+30, rHead, 1)
+		
+		"Pohyb na souřadnici věže a zvednutí do výšky podle počtu postavených kostek ve věži"
 		dType.SetPTPCmd(api, 2, vezX, vezY, vezZ+30+kostkaVel*pocet, rHead, 1)
+		
+		"Pokládání kostky na všek věže a uvolnění přísavky"
 		dType.SetPTPCmd(api, 2, vezX, vezY, vezZ+kostkaVel*pocet, rHead, 1)
 		dType.SetEndEffectorSuctionCup(api, 1,  0, 1)
 		dType.SetPTPCmd(api, 2, vezX, vezY, vezZ+30+kostkaVel*pocet, rHead, 1)
-		pocet=pocet+1
+		
+		pocet=pocet+1 ##Indexování počtu kostek postavených ve věži
+
 
 
